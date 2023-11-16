104742398403682b54e4c9f8bebab7043fcf4674
Aadesh Manandhar <aadeshmanandhar14646@gmail.com>
2023-11-16 12:10:11 +0545

    first fix check

--- src/components/Homepage/MealSection/MealBox.tsx
+++ src/components/Homepage/MealSection/MealBox.tsx
@@ -3,7 +3,7 @@
 import { useMemo } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { motion } from "framer-motion";
+import { motion, useAnimate } from "framer-motion";
 
 import { Meal } from "@/models/Meal";
 import { useWindowDimension } from "@/hooks/dimension";
@@ -16,57 +16,80 @@ interface Props {
 const MealBox: React.FC<Props> = ({ meal: { name, image, id }, index }) => {
   const { width } = useWindowDimension();
   const router = useRouter();
+  const [scope, animate] = useAnimate<HTMLDivElement>();
 
   const isMobile = useMemo(() => width < 500, [width]);
 
   const goToSpecificMenu = () => {
     router.push(`/menu?${id}`);
-  }
+  };
 
-  return (
-    <motion.div
-      onClick={goToSpecificMenu}
-      whileHover={
-        isMobile
-          ? {}
-          : {
-              x: [
-                -50 - index * 75,
-                -100 - index * 75,
-                -50 - index * 75 + (index !== 0 ? 0 : 25),
-              ],
-              scale: 1.15,
-              zIndex: 40,
-              rotateY: ["5deg", "10deg", "5deg", "0deg"],
-              boxShadow: "0px 0px 10px black",
-              backgroundColor: "rgb(212,142,93)",
-            }
-      }
-      whileTap={ isMobile ? {
+  const showTheCard = () => {
+    if (isMobile) {
+      animate(`#meal-${id}`, {
         scale: 1.15,
         position: "absolute",
-        left: (width / 4) + index * 75,
+        left: width / 4 + index * 75,
         top: "50px",
         width: "200px",
         zIndex: 50,
         boxShadow: "0px 0px 10px black",
         backgroundColor: "rgb(212, 142, 93)",
-      }: {}}
-      transition={isMobile ? { delay: 0.2 } : { stiffness: 100 }}
-      initial={{ x: -index * 75 }}
-      className="p-3 shadow-shadowLeft rounded-md bg-gradient-to-r from-[rgb(212,142,93)] to-[rgba(212,143,93,0.14)] text-center"
+      });
+    }
+  };
+
+  return (
+    <div
+      ref={scope}
+      onContextMenu={(event) => {
+        event.preventDefault();
+      }}
     >
-      <div className="w-44 h-44 overflow-hidden rounded-md">
-        <Image
-          src={image}
-          alt={name}
-          width={176}
-          height={176}
-          priority={true}
-        />
-      </div>
-      <span className="text-xl font-semibold text-center mt-4">{name}</span>
-    </motion.div>
+      <motion.div
+        id={`meal-${id}`}
+        onClick={goToSpecificMenu}
+        whileHover={
+          isMobile
+            ? {
+                scale: 1.15,
+                position: "absolute",
+                left: width / 4 + index * 75,
+                top: "50px",
+                width: "200px",
+                zIndex: 50,
+                boxShadow: "0px 0px 10px black",
+                backgroundColor: "rgb(212, 142, 93)",
+              }
+            : {
+                x: [
+                  -50 - index * 75,
+                  -100 - index * 75,
+                  -50 - index * 75 + (index !== 0 ? 0 : 25),
+                ],
+                scale: 1.15,
+                zIndex: 40,
+                rotateY: ["5deg", "10deg", "5deg", "0deg"],
+                boxShadow: "0px 0px 10px black",
+                backgroundColor: "rgb(212,142,93)",
+              }
+        }
+        transition={isMobile ? { delay: 0.2 } : { stiffness: 100 }}
+        initial={{ x: -index * 75 }}
+        className="p-3 shadow-shadowLeft rounded-md bg-gradient-to-r from-[rgb(212,142,93)] to-[rgba(212,143,93,0.14)] text-center"
+      >
+        <div className="w-44 h-44 overflow-hidden rounded-md">
+          <Image
+            src={image}
+            alt={name}
+            width={176}
+            height={176}
+            priority={true}
+          />
+        </div>
+        <span className="text-xl font-semibold text-center mt-4">{name}</span>
+      </motion.div>
+    </div>
   );
 };
 
